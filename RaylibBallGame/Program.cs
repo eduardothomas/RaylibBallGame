089f909012d251be3c2750350150ad150fb1edd1
Eduardo Thomas <eduardothomas64@gmail.com>
2024-09-02 16:53:24 -0500

    Initial Commit

---
+++ RaylibBallGame/Program.cs
@@ -0,0 +1,29 @@
+﻿using Raylib_cs;
+
+using static Raylib_cs.Raylib;
+
+namespace BallGame;
+
+class Program
+{
+    public static void Main()
+    {
+        int ballX = 400;
+        int ballY = 400;
+        
+        InitWindow(width: 1280, height:768, "My First Ball Game");
+        SetTargetFPS(60);
+
+        while (!WindowShouldClose())
+        {
+            ballX += 10;
+            
+            BeginDrawing();
+            ClearBackground(Color.SkyBlue);
+            
+            DrawCircle(ballX, 400, 20, Color.Red);
+            
+            EndDrawing();
+        }
+    }
+}
